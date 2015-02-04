65d0556e02d836e4c966d4fba8b7350233bbe1a7
Michael Hagedorn <michael@hagedorn.co.nz>
2015-02-05 05:54:49 +1300

    Started on skybox

---
+++ GameCore/Render/RenderObjects/ObjSkyBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameCore.Render.OpenGlHelper;
+using GameCore.Render.RenderMaterial;
+using OpenGL;
+
+namespace GameCore.Render.RenderObjects
+{
+    public class ObjSkyBox : ObjObject
+    {
+        public ObjSkyBox(Vector3[] vertexData, int[] elementData) : base(vertexData, elementData)
+        {
+        }
+
+        public ObjSkyBox(ObjectVectors anObjectVectors) : base(anObjectVectors)
+        {
+        }
+
+        public ObjSkyBox(List<string> lines, Dictionary<string, ObjMaterial> materials, int vertexOffset, int uvOffset) : base(lines, materials, vertexOffset, uvOffset)
+        {
+        }
+    }
+}
